ebc63ad67c63645dde6c905e7440bf7cfb63ea57
fucongcong <fucongcong@clothesmake.com>
2017-11-02 11:31:29 +0800

    移除无用配置

--- config/service.php
+++ config/service.php
@@ -8,23 +8,6 @@ return [
 
     //配置service
     'server' => [
-        'node_center' => [
-            'ip' => '127.0.0.1',
-            'serv' => '0.0.0.0',
-            'port' => 9518,
-            'config' => [
-                //'daemonize' => true,        
-                'worker_num' => 20,
-                'max_request' => 50000,
-                'task_worker_num' => 50,
-                'task_max_request' => 50000,
-                'heartbeat_idle_time' => 300,
-                'heartbeat_check_interval' => 60,
-                'dispatch_mode' => 3,
-                'log_file' => 'runtime/service/node_center.log',
-            ],
-            'public' => 'NodeCenter',
-        ],
         'monitor' => [
             //本机当前内网ip
             'ip' => '127.0.0.1',
@@ -32,9 +15,9 @@ return [
             'port' => 9517,
             'config' => [
                 'daemonize' => true,        
-                'worker_num' => 20,
+                'worker_num' => 2,
                 'max_request' => 50000,
-                'task_worker_num' => 50,
+                'task_worker_num' => 5,
                 'task_max_request' => 50000,
                 'heartbeat_idle_time' => 300,
                 'heartbeat_check_interval' => 60,
@@ -57,13 +40,13 @@ return [
             'port' => 9519,
             //server配置，请根据实际情况调整参数
             'config' => [
-                //'daemonize' => true,
+                'daemonize' => true,
                 //worker进程数量         
-                'worker_num' => 25,
+                'worker_num' => 2,
                 //最大请求数，超过后讲重启worker进程
                 'max_request' => 50000,
                 //task进程数量
-                'task_worker_num' => 50,
+                'task_worker_num' => 5,
                 //task进程最大处理请求上限，超过后讲重启task进程
                 'task_max_request' => 50000,
                 //心跳检测,长连接超时自动断开，秒
@@ -87,9 +70,9 @@ return [
             'port' => 9521,
             'config' => [
                 'daemonize' => true,        
-                'worker_num' => 25,
+                'worker_num' => 2,
                 'max_request' => 50000,
-                'task_worker_num' => 50,
+                'task_worker_num' => 5,
                 'task_max_request' => 50000,
                 'heartbeat_idle_time' => 300,
                 'heartbeat_check_interval' => 60,
